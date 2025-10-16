5f55e1b1e50e1819e6416383637f66ce6ad9b20d
nolan <144292887+nclan1@users.noreply.github.com>
2025-10-16 12:40:57 -0400

    added pubsub

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Printf("Error dialing ")
+	}
+	defer conn.Close()
+
+	fmt.Printf("Succesfully dialed")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("Error creating a channel")
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	select {
+	case <-signalChan:
+		fmt.Printf("Exiting...")
+	}
 }
